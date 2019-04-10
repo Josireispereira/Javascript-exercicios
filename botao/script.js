3bb2a707adfd0cd2e3a0e88b4b05c96718f6835b
josireispereira <josireispereira@gmail.com>
2019-04-10 14:03:17 -0300

    adiciona javascript

---
+++ botao/script.js
@@ -0,0 +1,17 @@
+function desliga(){
+    var meuBody = document.getElementById("fundo");
+
+    if(meuBody.style.background == "white"){
+        meuBody.style.background = "black";
+    }
+
+    else{
+        meuBody.style.background = "white";
+    }
+}
+
+// var gp1 = 
+
+// var gp2 = function(){
+//     document.getElementById
+// }
